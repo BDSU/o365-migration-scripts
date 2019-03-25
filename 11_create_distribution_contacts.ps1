711e6a07342450fa2028bdd47a968408e55de406
Roman Anasal <roman.anasal@bdsu.de>
2019-03-26 00:28:38 +0100

    Skript zum Erstellen der BDSU-Verteiler als Kontakte hinzugefügt
    Um die externen E-Mail-Adressen der JEs in einem Verteiler im
    BDSU-Tenant hinzufügen zu können, müssen diese als Kontakte angelegt
    sein, die man dann zu den Verteilern hinzufügen kann.

---
+++ 11_create_distribution_contacts.ps1
@@ -0,0 +1,24 @@
+﻿$credentials = Get-Credential
+$session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credentials -Authentication Basic -AllowRedirection
+Import-PSSession $session
+
+$city = Read-Host -Prompt "Stadt"
+$domain = Read-Host -Prompt "JE-Domain"
+
+$groups = @{
+    "$city - MIT"                = "bdsu.mit@$domain"
+    "$city - Finanzvorstand"     = "bdsu.fr@$domain"
+    "$city - IT-Ansprechpartner" = "bdsu.it@$domain"
+    "$city - QM-Ansprechpartner" = "bdsu.qm@$domain"
+    "$city - Vorstände"          = "bdsu.vorstaende@$domain"
+}
+
+$groups.Keys | ForEach-Object {
+    New-MailContact -DisplayName $_ -Name $_ -ExternalEmailAddress $groups[$_]
+}
+
+
+if ($Host.Name -eq "ConsoleHost") {
+    Write-Host "Press any key to exit..."
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+}
