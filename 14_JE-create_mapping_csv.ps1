9a8c41ff37fb4e178923b1fb81c4495fd2a246e6
Roman Anasal <roman.anasal@bdsu.de>
2019-05-16 20:45:48 +0200

    14_JE-create_mapping_csv: Warnungen ausgeben, wenn unerwartete Dateien dabei sind

--- 14_JE-create_mapping_csv.ps1
+++ 14_JE-create_mapping_csv.ps1
@@ -3,6 +3,11 @@
 $files = Get-ChildItem -Path $srcDir -Filter "*.pst" -name
 $domain = Read-Host -Prompt "Domain für Benutzernamen"
 
+$csv = Import-Csv -Path P:\import-users.csv
+$psts = $csv.'User Name' | ForEach-Object {
+    $_ -replace "@.*$",".pst"
+}
+
 $mapping = $files | ForEach-Object {
     $uid = $_ -replace ".pst$",""
     $userPrincipalName = "$uid@$domain"
@@ -22,7 +27,28 @@ $mapping = $files | ForEach-Object {
 
 $mapping | ft
 
+$diff = Compare-Object -ReferenceObject $psts -DifferenceObject $files
+
+$extras = $diff | Where-Object {$_.SideIndicator -eq "=>"}
+$missing = $diff | Where-Object {$_.SideIndicator -eq "<="}
+
+if ($missing) {
+    Write-Warning "Diese Dateien fehlen noch ($($missing.Count) Datei/en)"
+    $missing | ft -AutoSize InputObject
+} else {
+    Write-Host -ForegroundColor Green "Alle Postfächer exportiert"
+}
+
+if ($extras) {
+    Write-Warning "Diese Dateien gehören zu keinem bekannten Postfach"
+    $extras | ft -AutoSize InputObject
+}
+
 $csv = $mapping | ConvertTo-Csv -Delimiter "," -NoTypeInformation
 $csv = $csv -replace '"',''
 
-$csv | Out-File -Encoding utf8 "$srcDir/mapping.csv"
+#$csv | Out-File -Encoding utf8 "$srcDir/mapping.csv"
+
+if ($missing -or $extras) {
+    Write-Warning "Bitte überprüfe obenstehende Warnungen und verwende die mappings.csv nur, wenn diese geklärt sind; du kannst die mappings.csv ggf. manuell korrigieren."
+}
