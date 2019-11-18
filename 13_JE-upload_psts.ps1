58690bb7b2146c7a03897b37a8d825c54076ca9e
Roman Anasal <roman.anasal@bdsu.de>
2019-11-18 13:44:55 +0100

    13_JE-upload_psts: Fehlermeldung für Perfomance-Verbesserung hinzugefügt
    Bei den bereitgestellten VMs ist die Datenpartition bei der zweiten
    (3392) direkt eingebunden und als Netzlaufwerk freigegeben, das von der
    ersten (3391) ebenfalls als P:\ eingebunden wird.
    Wird der Upload auf der ersten VM gestartet, heißt das aber, dass die
    Daten erst über das Netzwerk von der zweiten abgerufen und dann
    hochgeladen werden. Damit wandern die Daten 3mal durch den kompletten
    Netzwerkstack...

--- 13_JE-upload_psts.ps1
+++ 13_JE-upload_psts.ps1
@@ -1,4 +1,11 @@
-﻿$srcDir = "P:\PSTs"
+﻿. "$PSScriptRoot\utils.ps1"
+
+$drive = Get-PSDrive P
+if (!$? -or !$drive -or $drive.DisplayRoot -like "\\data\data") {
+    error-exit "Für bessere Performance sollte dieses Skript auf der anderen VM (3392) ausgeführt werden"
+}
+
+$srcDir = "P:\PSTs"
 $logfile = "P:\azure_copy.log"
 $sasUrl = Read-Host -Prompt "SAS-URL"
 & 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe' /Y /source:$srcDir /V:$logfile /Dest:"$sasUrl" /Pattern:'*.pst'
